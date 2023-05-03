927d64980b067f14ab0d55c159822f08469fb29f
tondz <thanhtonvk@gmail.com>
2023-05-03 08:44:04 +0700

    first commit

---
+++ LoadData/Form1.cs
@@ -0,0 +1,63 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp;
+using LoadData.Models;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LoadData
+{
+    public partial class Form1 : Form
+    {
+        IFirebaseClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new FireSharp.FirebaseClient(config);
+            Thread t = new Thread(new ThreadStart(run));
+            t.Start();
+        }
+
+        SendNotification sendNotification = new SendNotification();
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "R20tmZqaTY9WnrsEr9vk5nyzq6rZ6hO4OACKD1Su",
+            BasePath = "https://wolfteam-f01f4-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        };
+
+        public void run()
+        {
+            try
+            {
+                BanLenhDAL banLenhDAL = new BanLenhDAL(client, sendNotification);
+                NewsDAL newsDal = new NewsDAL(client, sendNotification);
+                RateDAL rateDAL = new RateDAL();
+                while (true)
+                {
+                
+                    rateDAL.loadRate();
+                 
+                    banLenhDAL.loadBanLenh();
+               
+                    newsDal.addNormalNews();
+                
+                    newsDal.addNewsVip();
+                    Thread.Sleep(3000);
+                }
+            }
+            catch (Exception ex)
+            {
+                //label1.Text = ex.Message.ToString();
+                Console.WriteLine(ex.Message);
+                run();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LoadData/NewsDAL.cs
@@ -0,0 +1,134 @@
+﻿using FireSharp.Interfaces;
+using LoadData.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadData
+{
+    class NewsResponse
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Content { get; set; }
+        public Nullable<bool> Type { get; set; }
+    }
+
+    class NewsDAL
+    {
+        DBContext db;
+        WebClient client;
+        private IFirebaseClient firebaseClient;
+        private SendNotification sendNotification;
+
+        public NewsDAL(IFirebaseClient firebaseClient, SendNotification sendNotification)
+        {
+            db = new DBContext();
+            client = new WebClient();
+            this.firebaseClient = firebaseClient;
+            this.sendNotification = sendNotification;
+        }
+
+        public void addNormalNews()
+        {
+            Console.WriteLine("Load news");
+            string url =
+                "C:/Users/Administrator/AppData/Roaming/MetaQuotes/Terminal/Common/Files/Tin_tuc.csv";
+
+            client.DownloadFile(url, "TinTuc_New.txt");
+            var lineCountNewFile = File.ReadLines("TinTuc_New.txt").Count();
+
+            var lineCountOldFile = File.ReadLines("TinTuc.txt").Count();
+
+            if (lineCountNewFile != lineCountOldFile)
+            {
+                client.DownloadFile(url, "Tintuc.txt");
+                string values = "";
+                var lines = File.ReadLines("TinTuc.txt").ToList();
+                for (int i = lineCountOldFile; i < lines.Count(); i++)
+                {
+                    values += (lines[i] + "\n");
+                }
+
+                values.Replace("****", "**");
+                foreach (var value in values.Split(new[] {"**"}, StringSplitOptions.None))
+                {
+                    if (value.Trim().Length < 1) continue;
+                    else
+                    {
+                        db.AddNews(DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd")),
+                            TimeSpan.Parse(DateTime.Now.ToString("HH:mm")), value.Trim(), false);
+                        string ngay = DateTime.Now.ToString("yyyy-MM-dd");
+                        var model = db.News.ToList();
+                        model.Reverse();
+                        var news = model[0];
+                        var pushModel = new NewsResponse()
+                        {
+                            Content = news.Content,
+                            Date = news.Date.ToString("yyyy-MM-dd"),
+                            Id = news.Id,
+                            Time = news.Time.ToString("c"),
+                            Type = news.Type
+                        };
+                        firebaseClient.Set(@"News/" + ngay + "/" + pushModel.Id, pushModel);
+                        sendNotification.Send(pushModel.Content);
+                    }
+                }
+            }
+        }
+
+        public void addNewsVip()
+        {
+            Console.WriteLine("Load news vip");
+            string url =
+                "C:/Users/Administrator/AppData/Roaming/MetaQuotes/Terminal/Common/Files/Tin_tuc_vip.csv";
+
+            client.DownloadFile(url, "TinTuc_vip_New.txt");
+            var lineCountNewFile = File.ReadLines("TinTuc_vip_New.txt").Count();
+
+            var lineCountOldFile = File.ReadLines("Tin_tuc_vip.txt").Count();
+
+            if (lineCountNewFile != lineCountOldFile)
+            {
+                client.DownloadFile(url, "Tin_tuc_vip.txt");
+                string values = "";
+                var lines = File.ReadLines("Tin_tuc_vip.txt").ToList();
+                for (int i = lineCountOldFile; i < lines.Count(); i++)
+                {
+                    values += (lines[i] + "\n");
+                }
+
+                values.Replace("****", "**");
+                foreach (var value in values.Split(new[] {"**"}, StringSplitOptions.None))
+                {
+                    if (value.Trim().Length < 1) continue;
+                    else
+                    {
+                        db.AddNews(DateTime.Parse(DateTime.Now.ToString("yyyy-MM-dd")),
+                            TimeSpan.Parse(DateTime.Now.ToString("HH:mm")), value.Trim(), true);
+                        string ngay = DateTime.Now.ToString("yyyy-MM-dd");
+                        var model = db.News.ToList();
+                        model.Reverse();
+                        var news = model[0];
+                        var pushModel = new NewsResponse()
+                        {
+                            Content = news.Content,
+                            Date = news.Date.ToString("yyyy-MM-dd"),
+                            Id = news.Id,
+                            Time = news.Time.ToString("c"),
+                            Type = news.Type
+                        };
+                        firebaseClient.Set(@"NewsVip/" + ngay + "/" + pushModel.Id, pushModel);
+                        sendNotification.Send(pushModel.Content);
+                    }
+                }
+            }
+        }
+    }
+}
